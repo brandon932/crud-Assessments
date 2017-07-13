8ce3d3d3d68fba862a5636c8a7b3018e49debef1
brandon932 <bthornton_932@yahoo.com>
2017-07-13 02:10:49 -0600

    added update and cancel button

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -19,6 +19,21 @@ $('#assignment-submit').on("click", function(e){
     loadAssignments();
   });
 });
+$(document).on("click",".update-button", function(e){
+  e.preventDefault();
+  payload = {
+    name:$("#name").val(),
+    description:$("#description").val(),
+    tags:JSON.stringify($("#tags").val().split(","))
+  };
+  $.ajax({
+    method:"PUT",
+    url:"api/exercises/"+$(this).attr("id")
+  }).done(function(data){
+  $('.edit').hide();
+  $('.submit').show();
+  });
+});
 $(document).on("click",".delete-button", function(e){
   e.preventDefault();
   $.ajax({
@@ -33,23 +48,27 @@ $(document).on("click",".delete-button", function(e){
 
 $(document).on("click",".edit-button", function(e){
   e.preventDefault();
-  console.log("this is the edit click");
-
   $.get("api/exercises/"+$(this).attr("id"), function(data){
     $('#name').val(data.name);
-    $('#description').val(data.description);
-    $('#tags').val(data.tags);
+    $('#description').val(data.description) ;
+    $('#tags').val(data.tags) ;
+    // tags:JSON.stringify($("#tags").val().split(","))
   });
+  $('.submit').hide();
+  $('.edit').show();
+  $('.update-button').attr("id", $(this).attr("id"));
+
   //TODO: update submit button to update object instead of post it
 });
 function loadAssignments(){
+  $('.edit').hide();
   $.get("api/exercises", function(data){
     for (var i = 0; i < data.length; i++) {
       $("#all").prepend(
         "<tr>"+
         "<td>"+data[i].name+"</td>"+
         "<td>"+data[i].description+"</td>"+
-        "<td>"+data[i].tags+"</td>"+
+        "<td>"+ data[i].tags+"</td>"+
         '<td><a class="btn btn-danger btn-xs delete-button" id="'+data[i]._id+'" role="button">Delete</a>'+
         '&nbsp;<a class="btn btn-primary btn-xs edit-button" id="'+data[i]._id+'" role="button">&nbsp Edit &nbsp</a></td>'+
         "</tr>"
